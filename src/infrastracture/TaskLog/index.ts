728cf0b3b2c39a3b48dd15da78561a0b054c23df
sterashima <sterashima78@gmail.com>
2020-11-28 09:45:39 +0900

    fix type-check

--- src/infrastracture/TaskLog/index.ts
+++ src/infrastracture/TaskLog/index.ts
@@ -2,6 +2,7 @@ import * as firebase from "./Firebase/";
 import * as localStorage from "./LocalStorage";
 
 const obj =
+  //@ts-expect-error
   import.meta.env.VITE_STORAGE_TYPE === "localStorage"
     ? localStorage
     : firebase;
